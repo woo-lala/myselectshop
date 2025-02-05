dfd0c365c344ac28323af72c617559d526bceb00
woo-lala <hyunj2034@naver.com>
2025-02-05 22:04:37 +0900

    상품 페이징 및 정렬

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.sparta.myselectshop.repository;
 
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +12,5 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findALlByUser(User user);
 
+    Page<Product> findAllByUser(User user, Pageable pageable);
 }
